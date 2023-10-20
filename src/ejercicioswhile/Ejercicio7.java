8487278355812b2e62ed5c15b7fcf9e5f24b0ed1
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2023-10-20 14:38:40 +0200

    Ejercicio 7 terminado

--- src/ejercicioswhile/Ejercicio7.java
+++ src/ejercicioswhile/Ejercicio7.java
@@ -1,10 +1,56 @@
 package ejercicioswhile;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Ejercicio7 {
 
+	/* Vamos a crear el juego de adivina el número. El PC genera
+	 * un número de forma aleatoria y el usuario tendrá que
+	 * adivinar el número. Si falla, el PC le da la pista de si
+	 * el número que ha introducido es mayor o menor que el
+	 * número que tiene que adivinar. */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* Creamos el valor de referencia que generará el PC y el valor
+		 * que introducirá el usuario. */
+		final int RANDOM_NUM;
+		int userVal = 0;
+		
+		// Creamos el Scanner y el valor random.
+		Scanner sc = new Scanner(System.in);
+		Random rd = new Random();
+		
+		// Generamos un valor del 1 al 100.
+		RANDOM_NUM = rd.nextInt(1,100 + 1);
+		
+		// Le pedimos el valor al usuario.
+		System.out.println("El ordenador ha generado un número aleatorio." + "\n" +
+		"¿Serás capaz de adivinarlo?" + "\n" +
+		"Escribe el número que crees que el ordenador ha generado del 1 al 100.");
+		userVal = sc.nextInt();
+		
+		// Comprueba si el valor introducido no es igual que el valor generado.
+		while(userVal != RANDOM_NUM) {
+			
+			// Genera un bucle en el que...
+			
+			// Compruebas si el valor introducido es menor o mayor que el del PC.
+			if(userVal < RANDOM_NUM) {
+				// Dile al usuario que el valor es mayor y vuelve a pedirle el valor.
+				System.out.println("Fallaste. El valor es mayor.");
+				userVal = sc.nextInt();
+			} else if (userVal > RANDOM_NUM) {
+				// Dile al usuario que el valor es menor y vuelve a pedirle el valor.
+				System.out.println("Fallaste. El valor es menor.");
+				userVal = sc.nextInt();
+			}
+		}
+		
+		/* Imprime esto en pantalla si ha acertado el número. Ya
+		 * que si sale del bucle, significa que ha acertado. */
+		System.out.println("¡Acertaste! El valor es: " + RANDOM_NUM);
+		
+		// Cerramos el Scanner.
+		sc.close();
 	}
 
 }
